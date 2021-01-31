92fa64c0cac71501aad7235754f4c74ba2d25adf
Kirigakureno <78303776+Kirigakureno@users.noreply.github.com>
2021-01-31 19:34:58 +0300

    l

---
+++ Lab6/Lab6ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab6ex1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r, m1, m2, m3;
+            float t;
+            string n;
+            Console.WriteLine("Enter ID Number :");
+            r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Student Name :");
+            n = Console.ReadLine();
+            Console.WriteLine("Mark of Math : ");
+            m1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mark of Physics : ");
+            m2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mark of History : ");
+            m3 = Convert.ToInt32(Console.ReadLine());
+            t = m1 + m2 + m3;
+            
+            Console.WriteLine("Final Grade : " + (t/3));
+            
+            
+        }
+    }
+}
---
+++ Lab6/lab6ex3/Program.cs
@@ -0,0 +1,37 @@
+﻿class GFG
+{
+
+    static void printSum(int[] z,
+                    int[] x, int y)
+    {
+        int total = 0;
+
+
+        for (int i = 0; i < y; i++)
+        {
+            total += x[i];
+            total += (z[i] * 60);
+        }
+
+
+        Console.Write(total / 3600 + " : ");
+        total %= 3600;
+
+
+        Console.Write(total / 60 + ": ");
+        total %= 60;
+
+
+        Console.Write(total);
+    }
+
+
+    public static void Main()
+    {
+        int[] z = { 0, 2, 3, 2, 1 };
+        int[] x = { 45, 31, 11, 27, 28 };
+        int y = z.Length;
+
+        printSum(z, x, y);
+    }
+}
