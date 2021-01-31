ddc64ac89c99e592a9e3eb957f754a442a1cbcf1
Kirigakureno <78303776+Kirigakureno@users.noreply.github.com>
2021-01-31 19:31:48 +0300

    l

---
+++ Lab7/Lab7ex1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Lab7ex1
+{
+    public class lab7ex1
+    {
+
+
+        public static void pattern(int n)
+        {
+
+
+            for (int i = 1; i <= n * 2; i++)
+                Console.Write(" ");
+            Console.WriteLine(0 + "");
+
+
+            for (int i = 1; i <= (n * 2) - 1; i++)
+            {
+
+
+                if (i < n)
+                {
+                    for (int j = 1; j <= (n - i) * 2; j++)
+                        Console.Write(" ");
+                }
+                else
+                {
+                    for (int j = 1; j <= (i % n) * 2; j++)
+                        Console.Write(" ");
+                }
+
+                if (i < n)
+                {
+                    for (int j = 0; j <= i % n; j++)
+                        Console.Write(j + " ");
+                    for (int j = (i % n) - 1; j > 0; j--)
+                        Console.Write(j + " ");
+                    Console.Write(0);
+                }
+
+
+                else if (i > n)
+                {
+                    for (int j = 0; j <= n - (i - n); j++)
+                        Console.Write(j + " ");
+
+                    for (int j = (n - (i - n)) - 1; j > 0; j--)
+                        Console.Write(j + " ");
+                    Console.Write(0);
+                }
+                else
+                {
+                    for (int j = 0; j <= n; j++)
+                        Console.Write(j + " ");
+                    for (int j = n - 1; j > 0; j--)
+                        Console.Write(j + " ");
+                    Console.Write(0);
+                }
+                Console.WriteLine();
+            }
+
+
+            for (int i = 1; i <= n * 2; i++)
+                Console.Write(" ");
+            Console.Write(0);
+        }
+
+
+        public static void Main()
+        {
+            int n = 4;
+            pattern(n);
+        }
+    }
+}
