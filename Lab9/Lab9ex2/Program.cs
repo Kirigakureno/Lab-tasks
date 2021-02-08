232d419f0c1ccb9b520f41a98eb1f7b8fb98b8d0
Kirigakureno <78303776+Kirigakureno@users.noreply.github.com>
2021-02-08 14:46:05 +0300

    /

---
+++ Lab9/Lab9ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+
+class lab9ex2
+{
+
+    static float sumOfAP(float a,
+                         float d,
+                         float z)
+    {
+        float sum = (z / 2) *
+                    (2 * a +
+                    (z - 1) * d);
+        return sum;
+    }
+
+   
+    static public void Main()
+    {
+        float z = 15;
+        float a = 2.5f, d = 1.5f;
+        Console.WriteLine(sumOfAP(a, d, z));
+    }
+}
